3031ae69d675482ad309170a6b6bac28a9b4368a
Ben Clark <bennyjc01@gmail.com>
2021-11-14 15:17:33 +0800

    Overwrite knife damage mixin

--- src/main/java/com/youarethomas/arborealis/mixins/RecipeMixin.java
+++ src/main/java/com/youarethomas/arborealis/mixins/RecipeMixin.java
@@ -1,10 +1,14 @@
 package com.youarethomas.arborealis.mixins;
 
 import com.youarethomas.arborealis.Arborealis;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.collection.DefaultedList;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
@@ -13,7 +17,7 @@ import java.util.Random;
 @Mixin(Recipe.class)
 public interface RecipeMixin {
 
-    @ModifyArg(method = "getRemainder", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/collection/DefaultedList;set(ILjava/lang/Object;)Ljava/lang/Object;"), index = 1)
+    /*@ModifyArg(method = "getRemainder", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/collection/DefaultedList;set(ILjava/lang/Object;)Ljava/lang/Object;"), index = 1)
     private Object remainderDamage(Object object) {
         if (object instanceof ItemStack itemStack) {
             if (itemStack.isOf(Arborealis.CARVING_KNIFE)) {
@@ -22,5 +26,27 @@ public interface RecipeMixin {
             }
         }
         return object;
+    }*/
+
+    // TODO: There may be a better way to do this... see above
+
+    @Overwrite
+    default DefaultedList<ItemStack> getRemainder(Inventory inventory) {
+        DefaultedList<ItemStack> defaultedList = DefaultedList.ofSize(inventory.size(), ItemStack.EMPTY);
+
+        for(int i = 0; i < defaultedList.size(); ++i) {
+            Item item = inventory.getStack(i).getItem();
+            if (item.hasRecipeRemainder()) {
+                if (inventory.getStack(i).isOf(Arborealis.CARVING_KNIFE)) {
+                    ItemStack knifeStack = inventory.getStack(i);
+                    knifeStack.damage(1, new Random(), (ServerPlayerEntity)knifeStack.getHolder());
+                    defaultedList.set(i, knifeStack.copy());
+                } else {
+                    defaultedList.set(i, new ItemStack(item.getRecipeRemainder()));
+                }
+            }
+        }
+
+        return defaultedList;
     }
 }
