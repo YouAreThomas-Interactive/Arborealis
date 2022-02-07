8e8bc4e56ad23389838cdc9eae36fa5132779655
Steven <steven.ryan.ws@gmail.com>
2022-02-07 18:05:28 +0800

    Replaced stencil highlight
    - changed stencil highlight to make it more noticeable

--- src/main/java/gui/StencilBagScreen.java
+++ src/main/java/gui/StencilBagScreen.java
@@ -27,7 +27,7 @@ public class StencilBagScreen extends HandledScreen<StencilBagScreenHandler> {
         int y = (height - backgroundHeight) / 2;
         drawTexture(matrices, x, y, 0, 0, backgroundWidth, backgroundHeight);
 
-        drawTexture(matrices, x + 8, y + 18, 182, 18, 16, 16);
+        drawTexture(matrices, x + 7, y + 17, 182, 18, 16, 16);
     }
 
     @Override
