2b30cac6dacfbb2173c25ebc08937b4e1036043c
FishTreeMan <39683679+FishTreeMan@users.noreply.github.com>
2022-02-26 23:13:28 +0800

    Add tree structure conversion to and from NBT data.

--- src/main/java/com/youarethomas/arborealis/util/TreeStructure.java
+++ src/main/java/com/youarethomas/arborealis/util/TreeStructure.java
@@ -1,15 +1,23 @@
 package com.youarethomas.arborealis.util;
 
 import net.minecraft.block.Block;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.NbtHelper;
+import net.minecraft.nbt.NbtList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 /**
  * A tree object
  */
 public class TreeStructure {
+    private static final String TREE_STRUCTURE_LEAVES_NBT = "arborealis.treemanager.treestructure.leaves";
+    private static final String TREE_STRUCTURE_LOGS_NBT = "arborealis.treemanager.treestructure.logs";
+
     public static final int NAT_LEAVES_MIN = 12;
     public static final int LOGS_MIN = 2;
 
@@ -58,5 +66,65 @@ public class TreeStructure {
             }
         }
         return false;
+    }
+
+    /**
+     * Converts the tree structure into NBT data.
+     *
+     * @param structure The tree structure being converted.
+     * @return The NBT object representation of this tree structure.
+     */
+    public static NbtCompound toNbt(TreeStructure structure) {
+        NbtCompound nbt = new NbtCompound();
+
+        // Creates the NBT list for the leaves.
+        NbtList nbtLeafList = new NbtList();
+
+        for(BlockPos pos : structure.leaves) {
+            nbtLeafList.add(NbtHelper.fromBlockPos(pos));
+        }
+
+        nbt.put(TREE_STRUCTURE_LEAVES_NBT, nbtLeafList);
+
+        // Creates the NBT list for the logs.
+        NbtList nbtLogList = new NbtList();
+
+        for(BlockPos pos : structure.logs) {
+            nbtLogList.add(NbtHelper.fromBlockPos(pos));
+        }
+
+        nbt.put(TREE_STRUCTURE_LOGS_NBT, nbtLogList);
+
+        return nbt;
+    }
+
+    /**
+     * Creates a new tree structure with the given NBT data.
+     *
+     * @param nbt The given NBT data.
+     * @return The generated tree structure, null if it could not be determined.
+     */
+    public static TreeStructure fromNbt(NbtCompound nbt) {
+        TreeStructure structure = null;
+
+        if(nbt.contains(TREE_STRUCTURE_LEAVES_NBT) && nbt.contains(TREE_STRUCTURE_LOGS_NBT)) {
+            structure = new TreeStructure();
+
+            // Generates the set of leaves.
+            NbtList nbtLeafList = nbt.getList(TREE_STRUCTURE_LEAVES_NBT, NbtElement.COMPOUND_TYPE);
+
+            for (NbtElement element : nbtLeafList) {
+                structure.leaves.add(NbtHelper.toBlockPos((NbtCompound) element));
+            }
+
+            // Generates the set of logs.
+            NbtList nbtLogList = nbt.getList(TREE_STRUCTURE_LOGS_NBT, NbtElement.COMPOUND_TYPE);
+
+            for (NbtElement element : nbtLogList) {
+                structure.logs.add(NbtHelper.toBlockPos((NbtCompound) element));
+            }
+        }
+
+        return structure;
     }
 }
