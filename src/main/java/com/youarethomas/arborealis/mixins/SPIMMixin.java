ad2d29300ea43d5c4c01b32d565ec605027275a5
FishTreeMan <39683679+FishTreeMan@users.noreply.github.com>
2022-07-01 21:18:01 +0800

    chore: clean up imports.

--- src/main/java/com/youarethomas/arborealis/mixins/SPIMMixin.java
+++ src/main/java/com/youarethomas/arborealis/mixins/SPIMMixin.java
@@ -2,15 +2,11 @@ package com.youarethomas.arborealis.mixins;
 
 import com.youarethomas.arborealis.util.TreeManager;
 import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
