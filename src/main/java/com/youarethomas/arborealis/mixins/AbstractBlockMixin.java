ef332a6c4f0221d0b6095e5181adeaa10ebc1353
FishTreeMan <39683679+FishTreeMan@users.noreply.github.com>
2022-07-10 11:14:38 +0800

    fix: Accidental updates
    There was issue where the leaves would update metadata based on distance to logs.
    Now the update will only occur when the block changes.

--- src/main/java/com/youarethomas/arborealis/mixins/AbstractBlockMixin.java
+++ src/main/java/com/youarethomas/arborealis/mixins/AbstractBlockMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class AbstractBlockMixin {
     @Inject(method = "onStateReplaced(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Z)V", at = @At("HEAD"))
     public void onStateReplacedTree(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved, CallbackInfo ci) {
-        if(TreeManager.isTreeBlock(state) && !world.isClient()) {
+        if(!Registry.BLOCK.getId(state.getBlock()).equals(Registry.BLOCK.getId(newState.getBlock())) && TreeManager.isTreeBlock(state) && !world.isClient()) {
             ServerWorld serverWorld = (ServerWorld)world;
             TreeManager treeManager = TreeManager.getManager(serverWorld);
             treeManager.removeBlockFromTreeStructure(state, pos, serverWorld);
