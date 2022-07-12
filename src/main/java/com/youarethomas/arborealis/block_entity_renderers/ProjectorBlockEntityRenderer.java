7ec16614f0feb9ff07a69b5fcc2196ebb8bfb3f9
FishTreeMan <39683679+FishTreeMan@users.noreply.github.com>
2022-07-12 23:53:25 +0800

    feat: Dynamic beam shape render.
    Now it draws out the shape of the given rune on a block with beams.

--- src/main/java/com/youarethomas/arborealis/block_entity_renderers/ProjectorBlockEntityRenderer.java
+++ src/main/java/com/youarethomas/arborealis/block_entity_renderers/ProjectorBlockEntityRenderer.java
@@ -5,17 +5,21 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.youarethomas.arborealis.Arborealis;
 import com.youarethomas.arborealis.block_entities.ProjectorBlockEntity;
 import com.youarethomas.arborealis.rendering.BeamRenderLayer;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.client.render.*;
 import net.minecraft.client.render.block.entity.BeaconBlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Matrix3f;
 import net.minecraft.util.math.Matrix4f;
+import net.minecraft.util.math.Quaternion;
 
 public class ProjectorBlockEntityRenderer implements BlockEntityRenderer<ProjectorBlockEntity> {
     public static final Identifier BEAM_TEXTURE = new Identifier(Arborealis.MOD_ID, "textures/block/blank.png");
+    private static final float PIXEL_SIZE = 0.0625f;
 
     int[] test = {
             0, 0, 0, 0, 0, 0, 0,
@@ -33,12 +37,27 @@ public class ProjectorBlockEntityRenderer implements BlockEntityRenderer<Project
         matrices.push();
 
         if (entity.getLightLevel() > 0) {
-            matrices.translate(0.0, 1, 0.5);
-            renderBeamSegment(matrices, vertexConsumers.getBuffer(BeamRenderLayer.BEAM_RENDER_LAYER_TEXTURED), 1, 0.905f, 0.619f, 0.125f, 0, 1, -0.25f, -0.25f, 0.25f, -0.25f, -0.25f, 0.25f, 0.25f, 0.25f, 0, 1, 0, 1);
-            matrices.translate(0.6, 0, 0);
-            renderBeamSegment(matrices, vertexConsumers.getBuffer(BeamRenderLayer.BEAM_RENDER_LAYER_TEXTURED), 1, 0.905f, 0.619f, 0.125f, 0, 1, -0.25f, -0.25f, 0.25f, -0.25f, -0.25f, 0.25f, 0.25f, 0.25f, 0, 1, 0, 1);
-            matrices.translate(0.6, 0, 0);
-            renderBeamSegment(matrices, vertexConsumers.getBuffer(BeamRenderLayer.BEAM_RENDER_LAYER_TEXTURED), 1, 0.905f, 0.619f, 0.125f, 0, 1, -0.25f, -0.25f, 0.25f, -0.25f, -0.25f, 0.25f, 0.25f, 0.25f, 0, 1, 0, 1);
+            Direction facing = entity.getCachedState().get(HorizontalFacingBlock.FACING);
+            // Default is SOUTH.
+            float angle = 0.0f;
+
+            switch (facing) {
+                case NORTH -> angle = (float)Math.PI;
+                case WEST -> angle = (float)(Math.PI/2.0f);
+                case EAST -> angle = -(float)(Math.PI/2.0f);
+            }
+
+            matrices.translate(0.5, 0.5, 0.5);
+            matrices.multiply(Quaternion.fromEulerXyz((float)(Math.PI / 2.0), 0.0f, angle));
+            matrices.multiply(Quaternion.fromEulerXyz(0.0f, -(float)(Math.PI / 2.0), 0.0f));
+            matrices.translate(-0.5 + PIXEL_SIZE * 2, 0.5, -0.5 + PIXEL_SIZE * 2);
+
+            for(int ii = 0; ii < 49; ii++) {
+                matrices.push();
+                matrices.translate((float)(ii / 7) * PIXEL_SIZE * 2, 0.0f, (ii % 7) * PIXEL_SIZE * 2);
+                if(test[ii] == 1) renderBeamSegment(matrices, vertexConsumers.getBuffer(BeamRenderLayer.BEAM_RENDER_LAYER_TEXTURED), 1, 0.905f, 0.619f, 0.125f, 0, entity.getThrowDistance(), -PIXEL_SIZE, -PIXEL_SIZE, PIXEL_SIZE, -PIXEL_SIZE, -PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, 0, 1, 0, 1);
+                matrices.pop();
+            }
         }
 
         matrices.pop();
